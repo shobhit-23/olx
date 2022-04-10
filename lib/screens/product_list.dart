e722e9093b6436cff71173b357b70d6d1d3b314e
shobhit-23 <mshobhit286@gmail.com>
2022-04-11 03:49:51 +0530

    product_show++

--- lib/screens/product_list.dart
+++ lib/screens/product_list.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:olx/services/firebase_services.dart';
 import 'package:intl/intl.dart';
+import 'package:like_button/like_button.dart';
 
 class ProductList extends StatelessWidget {
   FirebaseService _service = FirebaseService();
@@ -48,29 +49,51 @@ class ProductList extends StatelessWidget {
                   return Container(
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      child: Stack(
                         children: [
-                          Container(
-                              height: 120,
-                              child: Center(
-                                child: Image.network(data['images'][0]),
-                              )),
-                          SizedBox(
-                            height: 5,
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                  height: 120,
+                                  child: Center(
+                                    child: Image.network(data['images'][0]),
+                                  )),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                _formattedprice,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                data['brand'],
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                // style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ],
                           ),
-                          Text(
-                            _formattedprice,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            data['brand'],
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            // style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
+                          Positioned(
+                            right: 0.0,
+                            child: LikeButton(
+                              circleColor: CircleColor(
+                                  start: Color(0xff00ddff),
+                                  end: Color(0xff0099cc)),
+                              bubblesColor: BubblesColor(
+                                dotPrimaryColor: Color(0xff33b5e5),
+                                dotSecondaryColor: Color(0xff0099cc),
+                              ),
+                              likeBuilder: (bool isLiked) {
+                                return Icon(
+                                  Icons.favorite,
+                                  color: isLiked ? Colors.red : Colors.grey,
+                                );
+                              },
+                            ),
+                          )
                         ],
                       ),
                     ),
